ef38e672115b976caca27201fd7ae5cd8ff45574
mohit <mohitmishra001.mm@gmail.com>
2023-12-06 18:24:16 +0530

    myenv added to gitignore

--- emailExtractor/myapp/ree.py
+++
@@ -1,9 +0,0 @@
-import re
-
-str1 = "sdfffaaaaaaaaaaaaaafsdaf mohit@gmail.com dfsdfdsf asdfsdfsadf mera naam mohit mishra hai aur meri id ye hai mohit@proton.me bur lohit ka email it hai lohit@hotmail.in"
-
-matches = re.finditer(r"\s\w*@\w*.\w*",str1)
-for match in matches:
-    print(match)
-
-
